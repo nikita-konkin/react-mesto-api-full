d20c411d4680bd7a99e428329053cccbf0c30812
Nikita Konkin <niki-konkin@yandex.ru>
2022-02-06 15:28:24 +0300

    bagifx

--- frontend/src/components/InfoTooltip.js
+++ frontend/src/components/InfoTooltip.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import regSucces from '../images/reg_succes.svg';
 import regError from '../images/reg_error.svg';
 
@@ -17,9 +17,9 @@ export default function InfoTooltip(props){
   }, [props.regStatus]);
 
   return (
- 
+
     <div className = {`popup ${props.isOpen ? 'popup_opened' : '' }`}>
-    
+
       <div className="popup__container">
         <button onClick={props.onClose} type="button" className={`popup__close-button `} />
 
